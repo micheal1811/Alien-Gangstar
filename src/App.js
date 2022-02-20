e652d086724790c13c37fa472175a3d51315ebd2
micheal1811 <65628229+micheal1811@users.noreply.github.com>
2022-02-20 22:26:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -97,6 +97,65 @@ export const StyledLink = styled.a`
   text-decoration: none;
 `;
 
+
+function App(){
+  //connecting
+const [accounts, setAccounts] = useState([]);
+
+async function connectAccounts(){
+  if (new WalletConnectProvider) {
+      const accounts = await  new WalletConnectProvider.request({
+          merhod: "eth_requestAccounts",
+       });
+      setAccounts(accounts);
+  } 
+}
+useEffect(() => {
+  connectAccounts();
+},[]);
+
+//minting 
+cont [mintAmount, setMintAmount] = useState(1);
+
+async function handleMint(){
+  if ( new WalletConnectProvider){
+    const provider = new WalletConnectProvider({
+      rpc: {
+        1: "https://cloudflare-eth.com/", // https://ethereumnodes.com/
+        137: "https://polygon-rpc.com/", // https://docs.polygon.technology/docs/develop/network-details/network/
+      },
+    });
+      const signer = provider.getsingner();
+      const contract = new ethrs.Contract(
+          abiaddress,
+          abi.abi,
+          signer
+      );
+      try{
+          const response = await contract.mint(BigNumber.from(mintAmount));
+          console.log("response:", response);
+      } catch (err) {
+          console.log("error: ", err);
+      }
+  }
+}
+
+//creating a button
+return (
+  <div className="App">
+    {accounts.length &&(
+      <div>
+        <button onClick={() => setMintAmount(mintAmount - 1)}>-</button>
+        {mintAmount}
+        <button onClick={() => setMintAmount(mintAmount + 1)}>+</button>
+        <button onClick={handleMint}>mintv</button>
+        </div>
+    )}
+  </div>
+);
+    }
+
+
 function App() {
   const dispatch = useDispatch();
   const blockchain = useSelector((state) => state.blockchain);
@@ -437,63 +496,6 @@ function App() {
     </s.Screen>
   );
 }
-
-function App(){
-  //connecting
-const [accounts, setAccounts] = useState([]);
-
-async function connectAccounts(){
-  if (new WalletConnectProvider) {
-      const accounts = await  new WalletConnectProvider.request({
-          merhod: "eth_requestAccounts",
-       });
-      setAccounts(accounts);
-  } 
-}
-useEffect(() => {
-  connectAccounts();
-},[]);
-
-//minting 
-cont [mintAmount, setMintAmount] = useState(1);
-
-async function handleMint(){
-  if ( new WalletConnectProvider){
-    const provider = new WalletConnectProvider({
-      rpc: {
-        1: "https://cloudflare-eth.com/", // https://ethereumnodes.com/
-        137: "https://polygon-rpc.com/", // https://docs.polygon.technology/docs/develop/network-details/network/
-      },
-    });
-      const signer = provider.getsingner();
-      const contract = new ethrs.Contract(
-          abiaddress,
-          abi.abi,
-          signer
-      );
-      try{
-          const response = await contract.mint(BigNumber.from(mintAmount));
-          console.log("response:", response);
-      } catch (err) {
-          console.log("error: ", err);
-      }
-  }
-}
-
-//creating a button
-return (
-  <div className="App">
-    {accounts.length &&(
-      <div>
-        <button onClick={() => setMintAmount(mintAmount - 1)}>-</button>
-        {mintAmount}
-        <button onClick={() => setMintAmount(mintAmount + 1)}>+</button>
-        <button onClick={handleMint}>mintv</button>
-        </div>
-    )}
-  </div>
-);
-    }
 
 
 export default App;
