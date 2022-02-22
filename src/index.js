e88f2e5d8ab0235562f3ce33a6594677433e6b9d
micheal1811 <65628229+micheal1811@users.noreply.github.com>
2022-02-22 13:56:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import App from "./walletconnect";
+import walletconnect from "./walletconnect";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
