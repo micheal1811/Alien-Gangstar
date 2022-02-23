3776b4ac8d446310b57c64f38f863799f314be56
micheal1811 <65628229+micheal1811@users.noreply.github.com>
2022-02-23 13:16:06 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ ReactDOM.render(
 );
 
 
+ReactDOM.render(
+  <Provider walletconnect={walletconnect}>
+  <walletconnect />
+</Provider>,
+  document.getElementById("connectwallet")
+);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
